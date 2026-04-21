29ae27102e8e2936786693a648e5e3bde22c96ad
Tvomato <JustinLi2013@gmail.com>
2026-04-20 22:04:26 -0400

    wip: prepare backend for deploy

--- backend/db_config.py
+++ backend/db_config.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 def get_db() -> str:
     load_dotenv()
 
+    url = os.environ.get("DATABASE_URL")
+    if url:
+        return url
+
     DB_USER = os.environ.get("DB_USER")
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
     DB_HOST = os.environ.get("DB_HOST")
